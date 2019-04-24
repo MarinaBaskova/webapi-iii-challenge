1664f0fd73a781f42233d9bbfdb3a7ab5b5f0cfe
Marina Baskova <marinabaskova@MacBook-Pro-11598.local>
2019-04-24 12:54:19 -0700

    added posts route, added morgan and helmet mw

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const postsRouter = require('./posts/postsRouter.js');
+// const usersRouter = require('./users/usersRouter');
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.get('/', (req, res) => {
+	res.send('<h2>Welcome to Posts App</h2>');
+});
+
+server.use('/api/posts', postsRouter);
+// server.use('/api/users', usersRouter);
+
+module.exports = server;
